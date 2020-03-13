091ba6fa333560ef2fdac350b5a1159c3c32f75d
chathula <chathula@different.com.au>
2020-03-13 13:19:06 +0530

    update the response text

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-  it("respond with Hello World", () => {
-    request(app).get("/").expect("Hello World");
+  it("respond with Hello World", (done) => {
+    request(app).get("/").expect("Hello World", done);
   })
 });
